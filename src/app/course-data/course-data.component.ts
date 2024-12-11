9ca7a2216c44ab5ec567e54920e4f806128e2d8a
vaishnavi22-s4s <vaishnvi22.s4s@gmail.com>
2024-12-11 17:55:12 +0530

    accordion add

--- src/app/course-data/course-data.component.ts
+++ src/app/course-data/course-data.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-course-data',
@@ -6,48 +7,88 @@ import { Component } from '@angular/core';
   styleUrl: './course-data.component.scss'
 })
 export class CourseDataComponent {
+[x: string]: any;
+
   youTube=[
     {
-      videoUrl:"https://www.youtube.com/embed/daF9R6dadH8",
+      videoUrl:"https://www.youtube.com/watch?v=bvxiJ5fZe3A",
       logoUrl:"../../assets/whychimg2.webp",
       videoTitle:"Intro-Class",
-      channelName:"Prettify",
-      views:"1M views • 87 days ago"
+      classno:"class-1",
     },
     {
-      videoUrl:"../../assets/video.mp4",
+      videoUrl:"https://www.youtube.com/watch?v=NChgXc5cIbI",
       logoUrl:"../../assets/whychimg2.webp",
       videoTitle:"Aari Journey",
-      channelName:"Prettify",
-      views:"6M views • 58 days ago"
+      classno:"class-2",
+     
     },
     {
-      videoUrl:"../../assets/video.mp4",
+      videoUrl:"https://www.youtube.com/watch?v=OcvBS4nuX_A",
       logoUrl:"../../assets/whychimg2.webp",
       videoTitle:"StitchPath",
-      channelName:"Prettify",
-      views:"5M views • 25 days ago"
-    },{
-      videoUrl:"../../assets/video.mp4",
+      classno:"class-3",
+ 
+    },
+    {
+      videoUrl:"https://www.youtube.com/watch?v=T8BfsP5tZUc",
       logoUrl:"../../assets/whychimg2.webp",
       videoTitle:"AariSteps",
-      channelName:"Prettify",
-      views:"3M views • 8 days ago"
+      classno:"class-4",
+     
     },
     {
-      videoUrl:"../../assets/video.mp4",
+      videoUrl:"https://www.youtube.com/watch?v=9fnQ8VmfH4Q",
       logoUrl:"../../assets/whychimg2.webp",
       videoTitle:"ThreadWay",
-      channelName:"Prettify",
-      views:"4M views • 4 days ago"
+      classno:"class-5",
+    
     },
     {
-      videoUrl:"https://www.youtube.com/embed/daF9R6dadH8",
+      videoUrl:"https://www.youtube.com/watch?v=oTbrKdyvVeY",
       logoUrl:"../../assets/whychimg2.webp",
       videoTitle:"DesignRise",
-      channelName:"Prettify",
-      views:"7M views • 1 days ago"
+      classno:"class-6",
+      
+    },
+    {
+      videoUrl:"https://www.youtube.com/watch?v=73JkxmZLHUA",
+      logoUrl:"../../assets/whychimg2.webp",
+      videoTitle:"ThreadWay",
+      classno:"class-7",
+      
+    },
+    {
+      videoUrl:"https://www.youtube.com/watch?v=bvxiJ5fZe3A",
+      logoUrl:"../../assets/whychimg2.webp",
+      videoTitle:"DesignRise",
+      classno:"class-8",
+    
     }
   ]
+  constructor(private sanitizer: DomSanitizer) { }
+  sanitizeUrl(url: string): SafeResourceUrl {
+    const videoId = this.extractVideoId(url);
+    const embedUrl = `https://www.youtube.com/embed/${videoId}?autoplay=1`;
+    return this.sanitizer.bypassSecurityTrustResourceUrl(embedUrl);
+  }
+
+private extractVideoId(url: string): string | null {
+  const regex = /(?:https?:\/\/)?(?:www\.)?youtube\.com\/.*v=([^&\s]+)|youtu\.be\/([^&\s]+)/;
+  const match = url.match(regex);
+  return match ? match[1] || match[2] : null;
+}
+
+items:any;
+itemDetail(item:any){
+
+  this.items = item;
+
+}
+
+
+
+
+
 
 }
